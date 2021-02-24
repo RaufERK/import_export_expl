364df392b068656694697e3d83cb7ca2ea0f76e0
Rauf Erk <erk.rauf@gmail.com>
2021-02-24 15:07:44 +0300

    init

---
+++ file_to_export.js
@@ -0,0 +1,9 @@
+const foo1 = (e) => {
+  console.log('foo1 =>', e);
+};
+
+const foo2 = (e) => {
+  console.log('foo2 =>', e);
+};
+
+export { foo1, foo2 };
---
+++ main.js
@@ -0,0 +1,5 @@
+import { foo1, foo2 } from './file_to_export.js';
+
+
+foo1('TEXT 1')
+foo2('TEXT 2')
